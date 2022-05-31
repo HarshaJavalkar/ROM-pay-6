23f8923cea368cd313cffe73ebc729032b1b7a6d
harsha javalkar <HarshaJavalkar>
2022-05-31 14:24:20 +0530

    payment

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+insert into CREDIT_CARD values (1234567,5000);
+insert into CREDIT_CARD values (1234569,5000);
+insert into CREDIT_CARD values (9876543,100000);
+insert into CREDIT_CARD values (2345678,1000);
+insert into CREDIT_CARD values (3456789,2000);
+insert into CREDIT_CARD values (4567890,5000);
